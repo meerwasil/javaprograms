43e71ee3cd6a7c549c50ecdf6c5b20aa6d46bd89
meerwasil <96772743+meerwasil@users.noreply.github.com>
2022-06-01 22:11:34 +0530

    Add files via upload

---
+++ Fruit.java
@@ -0,0 +1,24 @@
+public class Fruit {
+    private String name;
+    private String colour;
+    public void setname(String n)
+    {
+        name = n;
+
+
+    }
+    public void setcolour(String s)
+    {
+        colour = s;
+    }
+    public String getname()
+    {
+        return name;
+
+    }
+    public String getcolour()
+    {
+        return colour;
+
+    }
+}
---
+++ FruitTest.java
@@ -0,0 +1,11 @@
+
+
+public class FruitTest {
+    public static void main(String[] args) {
+
+        Fruit obj = new Fruit();
+        obj.setname("Apple");
+        obj.setcolour("Red");
+       System.out.println("name " + obj.getname() + "\t colour " + obj.getcolour());
+    }
+}
